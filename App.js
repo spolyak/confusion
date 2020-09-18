83b1064523069427afd188c351ab0f109159a820
spolyak <spolyak@digitalartefacts.com>
2020-09-18 10:37:25 -0500

    update

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Try this next line as well</Text>
       <StatusBar style="auto" />
     </View>
   );
