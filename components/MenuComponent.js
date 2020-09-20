c0f4e816d04ba465c19e9369c272a89c57a73fd0
spolyak <stevepolyak@mac.com>
2020-09-19 22:28:12 -0500

    update with main component

---
+++ components/MenuComponent.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
+import { Avatar, ListItem } from 'react-native-elements';
+
+function Menu(props) {
+
+
+    const renderMenuItem = ({ item, index }) => {
+        return (
+            <ListItem key={index}>
+                <Avatar ssize="small"
+                    rounded
+                    title="MT"
+                />
+                <ListItem.Content>
+                    <ListItem.Title>{item.name}</ListItem.Title>
+                    <View style={styles.subtitleView}>
+                        <Text >{item.description}</Text>
+                    </View>
+                </ListItem.Content>
+            </ListItem>
+        );
+    }
+    styles = StyleSheet.create({
+        subtitleView: {
+            flexDirection: 'row',
+            paddingLeft: 10,
+            paddingTop: 5
+        },
+        ratingImage: {
+            height: 19.21,
+            width: 100
+        },
+        ratingText: {
+            paddingLeft: 10,
+            color: 'grey'
+        }
+    })
+
+    return (
+        <FlatList
+            data={props.dishes}
+            renderItem={renderMenuItem}
+            keyExtractor={item => item.id.toString()}
+        />
+    )
+
+
+
+}
+
+
+export default Menu;
