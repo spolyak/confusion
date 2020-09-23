f45859fd80650a2d0bfea64cd0b42d65eef84ed6
spolyak <stevepolyak@mac.com>
2020-09-22 20:20:17 -0500

    add home screen configuration

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+
+
+function RenderItem(props) {
+  const item = props.item;
+
+  if (item != null) {
+    return (
+      <Card>
+        <Card.Image source={require('./images/uthappizza.png')} />
+        <Card.Title>{item.name}</Card.Title>
+        <Card.Title>{item.designation}</Card.Title>
+        <Card.Divider />
+        <Text style={{ margin: 10 }}>
+          {item.description}
+        </Text>
+      </Card>
+    );
+  }
+  else {
+    return (
+      <View></View>
+    );
+  }
+}
 
 class HomeComponent extends Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+    };
+  }
 
-    render() {
-        return (
-            <View>
-              <Text>Home Screen</Text>
-            </View>
-          );
-    }
+  render() {
+    return (
+      <ScrollView>
+        <RenderItem item={this.state.dishes.filter((dish) => dish.featured)[0]} />
+        <RenderItem item={this.state.promotions.filter((promotion) => promotion.featured)[0]} />
+        <RenderItem item={this.state.leaders.filter((leader) => leader.featured)[0]} />
+      </ScrollView>
+    );
+  }
 }
 
 export default HomeComponent;
